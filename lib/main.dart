e6813275fd44a47f403d2bda8786d654b09c3156
milindahashan50 <milindahashan50@gmail.com>
2025-08-23 08:48:17 +0000

    create simple demo  app

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(
+    MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text("My App", style: TextStyle(color: Colors.deepPurple),), elevation: 0.9, backgroundColor: Colors.amber,),
+      body: Center(child: Text("Hello", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold))),),),);  
+} 
