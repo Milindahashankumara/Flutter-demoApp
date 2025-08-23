2970efb66b2220d67bf0a1eefda3a7c298b7ed3e
milindahashan50 <milindahashan50@gmail.com>
2025-08-23 08:59:47 +0000

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -6,5 +6,5 @@ void main() {
     MaterialApp(
     home: Scaffold(
       appBar: AppBar(title: Text("My App", style: TextStyle(color: Colors.deepPurple),), elevation: 0.9, backgroundColor: Colors.amber,),
-      body: Center(child: Text("Hello", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold))),),),);  
+      body: Center(child: Text("Hello", style: TextStyle(fontSize: 32,))),),),);  
 } 
