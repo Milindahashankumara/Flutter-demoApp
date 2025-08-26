892f465a2c3907f21e3fd9d00471d10c79de6833
milindahashan50 <milindahashan50@gmail.com>
2025-08-26 08:56:59 +0000

    use statefullwidget for increase values

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
   runApp(
-    MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text("My App", style: TextStyle(color: Colors.deepPurple),), elevation: 0.9, backgroundColor: Colors.amber,),
-      body: Center(child: Text("Hello", style: TextStyle(fontSize: 32,))),),),);  
-} 
+  const App()
+  );
+}
+
+
+class App extends  StatelessWidget {
+const App({super.key});
+
+  @override
+  Widget build  (BuildContext context) {
+   
+    return  MaterialApp(
+      home: Scaffold(
+         body: Body(),
+        appBar: AppBar(
+          title: Text(
+            "My App",
+            style: TextStyle(color: Colors.deepPurple),
+          ),
+          elevation: 0.9,
+          backgroundColor: Colors.amber,
+        ),
+       
+      ),
+    );
+}
+}
+
+
+
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int value = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text("Value - $value"),
+          TextButton(onPressed: () {
+            setState(() {
+              value++;
+            });
+          }, child: Text("Press"))
+        ],
+      ),
+    );
+  }
+}
+
+
+
